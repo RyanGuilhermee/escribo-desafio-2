a14e554e37cd841a39aba8f5e090acd17b4ae562
Ryan <ryan.guilherme.740@gmail.com>
2023-12-01 14:35:16 -0300

    feat: implements books repository

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,24 @@
+import 'package:desafio_escribo_2/src/data/http/http_client.dart';
+import 'package:desafio_escribo_2/src/pages/stores/books_store.dart';
+import 'package:desafio_escribo_2/src/repositories/books_repository.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final BooksStore store =
+      BooksStore(repository: BooksRepository(client: HttpClient()));
+
+  @override
+  void initState() {
+    super.initState();
+    store.getBooks();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,53 +30,86 @@ class HomePage extends StatelessWidget {
               style: TextStyle(color: Colors.white),
             ),
             backgroundColor: Colors.deepPurple),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 0, 5, 0),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Livros'),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Favoritos'),
-                  ),
-                ],
-              ),
-              const Divider(),
-              Expanded(
-                  child: GridView.count(
-                crossAxisCount: 2,
-                children: List.generate(9, (index) {
-                  return Center(
-                    child: Column(children: [
-                      SizedBox(
-                        height: 125,
-                        width: 125,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.transparent,
-                            elevation: 0,
+        body: AnimatedBuilder(
+          animation:
+              Listenable.merge([store.isLoading, store.error, store.state]),
+          builder: (context, child) {
+            if (store.isLoading.value) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (store.error.value.isNotEmpty) {
+              return Center(
+                  child: Text(
+                store.error.value,
+                style: const TextStyle(color: Colors.red),
+              ));
+            }
+
+            if (store.state.value.isEmpty) {
+              return const Center(
+                child: Text('Nenhum livro encontrado'),
+              );
+            } else {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Livros'),
                           ),
-                          child: Image.network(
-                              'https://www.gutenberg.org/cache/epub/72134/pg72134.cover.medium.jpg'),
                         ),
-                      ),
-                      const Text('Titulo'),
-                      const Text('Autor'),
-                    ]),
-                  );
-                }),
-              ))
-            ],
-          ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Favoritos'),
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                    Expanded(
+                        child: GridView.count(
+                      crossAxisCount: 2,
+                      children:
+                          List.generate(store.state.value.length, (index) {
+                        final book = store.state.value[index];
+
+                        return Center(
+                          child: Column(children: [
+                            SizedBox(
+                              height: 125,
+                              width: 125,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.transparent,
+                                  elevation: 0,
+                                ),
+                                child: Image.network(book.coverUrl),
+                              ),
+                            ),
+                            Text(book.title,
+                                style: const TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                    overflow: TextOverflow.ellipsis)),
+                            Text(
+                              book.author,
+                              style: const TextStyle(fontSize: 10),
+                            ),
+                          ]),
+                        );
+                      }),
+                    ))
+                  ],
+                ),
+              );
+            }
+          },
         ));
   }
 }
